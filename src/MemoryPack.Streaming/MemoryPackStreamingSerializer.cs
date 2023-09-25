4b27cd60ecdd8ed8f0a0dab174a74a0894d1c242
이승후 [gadlsh1208] <gadlsh1208@nexon.co.kr>
2023-09-25 14:21:48 +0900

    Complete PipeReader when cancelled

--- src/MemoryPack.Streaming/MemoryPackStreamingSerializer.cs
+++ src/MemoryPack.Streaming/MemoryPackStreamingSerializer.cs
@@ -154,6 +154,7 @@ public static class MemoryPackStreamingSerializer
 
                     if (readResult.IsCanceled)
                     {
+                        await pipeReader.CompleteAsync().ConfigureAwait(false);
                         yield break;
                     }
 
